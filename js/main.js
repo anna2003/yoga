1ad12682236d6cd16868e8f522d2d63fb32b6a13
Anna <ryavkina@mail.ru>
2019-07-31 12:35:21 +0500

    Задание 10

--- js/main.js
+++ js/main.js
@@ -32,4 +32,43 @@ window.addEventListener("DOMContentLoaded",function(){
             }
         }
     });
+    
+    let deadline = '2019-7-31';
+
+    function getTimeRemaining(endTime){
+        let t = Date.parse(endTime) - Date.parse(new Date()),
+            seconds = Math.floor(t / 1000 % 60),
+            minutes = Math.floor(t / 1000 / 60 % 60),
+            hours = Math.floor(t / 1000 / 3600);
+        return {
+            'seconds' : seconds,
+            'minutes' : minutes,
+            'hours' : hours,
+            'total' : t
+        };       
+    }
+    function setClock(id, endTime){
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock(){
+            let t = getTimeRemaining(endTime);
+            hours.textContent = t.hours > 9 ? t.hours: `0${t.hours}`;
+            minutes.textContent = t.minutes > 9 ? t.minutes: `0${t.minutes}`;
+            seconds.textContent = t.seconds> 9 ? t.seconds: `0${t.seconds}`;
+            
+            if(t.total <= 0){
+                clearInterval(timeInterval);
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+            }
+        }
+
+    };
+    setClock('timer',deadline);
+
 });
